bd32defdb55d0825d0be80ac8be3ebe7710db2e0
Lucas Siqueira <lucassiqueira0763@hotmail.com>
2023-11-21 00:42:31 +0000

    Formulário dos contatos

---
+++ Projeto_1/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+  //Função que preencher automaticamente o solecct com vários <option>
+  //Função vai ser aquiiii
+  fillBirthDateSelect();
+
+});
+
+
+//Evita que o formulário vá para outro lugar clicar no submit
+document.addEventListener("submit", function(event){
+  event.preventDefault();
+  if(validateForm()){
+    
+  }
+});
+
+//Esta função vai preencher automaticamente o select
+function fillBirthDateSelect(){
+  var currentYear = new Date().getFullYear();
+  var select = document.getElementById("birthYear");
+  for(var year = 1900; year <= currentYear; year++) {
+    var option = document.createElement("option");
+    option.value = year;
+    option.text = "Ano " + year;
+    //Vai ao parent <select> e cria várias options umas por baixo das outras
+    select.appendChild(option);
+  }
+}
